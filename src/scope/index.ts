3eae4f5d88e38ad10bd0cccd5b9ce5cbd330040f
AI-VERSE <hello@bogdanpascal.com>
2026-09-10 20:54:28 +0300

    feat: export Data scope boundary

---
+++ src/scope/index.ts
@@ -0,0 +1,8 @@
+export * from "./errors.js";
+export * from "./types.js";
+export {
+  TrustedDataRoot,
+  createStandaloneDataScope,
+  createWorkspaceDataScope,
+  openScopedDataDatabase,
+} from "./trusted-root.js";
