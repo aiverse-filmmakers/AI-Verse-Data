7bf6a5df0d7f7aefacc9ad147acd6737d371794b
AI-VERSE <hello@bogdanpascal.com>
2026-09-11 17:42:58 +0300

    feat: export native workspace resolver

--- src/native/index.ts
+++ src/native/index.ts
@@ -2,3 +2,5 @@ export * from "./types.js";
 export { AiVerseOsCompatibilityDetector } from "./compatibility.js";
 export * from "./extension-types.js";
 export { AiVerseDataExtensionInstaller } from "./extension-installer.js";
+export * from "./workspace-types.js";
+export { AiVerseDataWorkspaceManager } from "./workspace.js";
